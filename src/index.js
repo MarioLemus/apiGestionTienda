54e50623d3ec74f9d9dada0e8ae0545a88b8a26f
MarioLemus <theliarnorland@gmail.com>
2024-06-25 17:08:46 -0600

    quit unuse comments

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import { config as envConfig } from 'dotenv'
 import config from 'config'
 import { connectDB } from './utils/connectDB.js'
 import userRoutes from './routes/user.routes.js'
-//import fs from 'fs'
-//import path from 'path'
 
 envConfig()
 connectDB()
@@ -14,10 +12,6 @@ const app = express()
 const port = config.get('server.port')
 const devClient = 'http://localhost:5173'
 
-// eslint-disable-next-line no-undef
-//const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
-
-//app.use(logger('combined', { stream: accessLogStream }))
 app.use(express.json())
 app.use(logger(config.get('logger')))
 app.use(cors({

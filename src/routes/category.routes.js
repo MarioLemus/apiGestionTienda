8adec7d6c80f083cc4f4f9242d08275a2a797d7d
MarioLemus <theliarnorland@gmail.com>
2024-06-29 12:05:42 -0600

    change linting styles

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -5,11 +5,8 @@ const router = Router()
 const categoryController = new CategoryController()
 
 router.post('/create-category', categoryController.post)
-
 router.put('/edit-category/:_id', categoryController.put)
-
 router.delete('/delete-category/:_id', categoryController.delete)
-
 router.get('/get-category', categoryController.get)
 
 export default router

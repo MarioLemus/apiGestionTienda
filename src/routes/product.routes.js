01a4e0e1333f2f8440181abdb7fe08b7df8c7a7a
MarioLemus <theliarnorland@gmail.com>
2024-06-29 12:05:54 -0600

    change linting styles

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -4,10 +4,10 @@ import { ProductController } from '../controllers/product.controller.js'
 const router = Router()
 const productController = new ProductController()
 
-router.post("/create-product", productController.create);
-router.put("/edit-product/:_id", productController.edit);
-router.delete("/delete-product/:_id", productController.delete);
-router.get("/get-product", productController.get);
-router.get("/getbyname/:name", productController.getbyname);
+router.post('/create-product', productController.create)
+router.put('/edit-product/:_id', productController.edit)
+router.delete('/delete-product/:_id', productController.delete)
+router.get('/get-product', productController.get)
+router.get('/getbyname/:name', productController.getbyname)
 
 export default router

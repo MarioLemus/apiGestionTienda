7fcc470332689462c06b99a988f503512b38b25b
MarioLemus <theliarnorland@gmail.com>
2024-06-29 12:06:26 -0600

    add validateToken middleware to routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/user.controller.js'
+import { validateToken } from '../middlewares/validateToken.js'
 
 const router = Router()
 
-router.get('/user', UserController.getAll)
-router.post('/user', UserController.create)
-router.get('/user/:id', UserController.getOne)
-router.put('/user/:id', UserController.updateOne)
-router.delete('/user/:id', UserController.deleteOne)
+router.get('/user', validateToken, UserController.getAll)
+router.post('/user', validateToken, UserController.create)
+router.get('/user/:id', validateToken, UserController.getOne)
+router.put('/user/:id', validateToken, UserController.updateOne)
+router.delete('/user/:id', validateToken, UserController.deleteOne)
 
 export default router

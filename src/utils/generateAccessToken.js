a35f0fa5c4520b2bc37d35586d3ae63ff537733e
MarioLemus <theliarnorland@gmail.com>
2024-06-30 13:10:30 -0600

    add util

--- src/utils/generateAccessToken.js
+++ src/utils/generateAccessToken.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import config from 'config'
 
-export const generateAccessToken = (user, role) => {
-  return jwt.sign({ user, role }, config.get('tk.secret'), { expiresIn: config.get('tk.exp') })
+export const generateAccessToken = (userID, role) => {
+  return jwt.sign({ userID, role }, config.get('tk.secret'), { expiresIn: config.get('tk.exp') })
 }

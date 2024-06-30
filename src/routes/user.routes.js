40c8c8cff2210703ac82bfa7bc94f6249da7308c
Milton2003AFG <miltonflores2003@gmail.com>
2024-06-29 20:39:27 -0600

    Cambios en userController

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express'
 import { UserController } from '../controllers/user.controller.js'
+import { getMulterStorageConfig }from '../utils/getMulterStorageConfig.js'
+import multer from 'multer'
 
+const storageConfig = await getMulterStorageConfig();
+const upload = multer({ storage: storageConfig });
 const router = Router()
 
 router.get('/user', UserController.getAll)
-router.post('/user', UserController.create)
+router.post('/user', upload.single('profilePic'), UserController.create)
 router.get('/user/:id', UserController.getOne)
-router.put('/user/:id', UserController.updateOne)
+router.put('/user/:id', upload.single('profilePic'), UserController.updateOne)
 router.delete('/user/:id', UserController.deleteOne)
 
 export default router

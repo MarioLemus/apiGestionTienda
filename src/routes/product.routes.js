b69d4bc9f899da1838db12014e63c2b933e1ab24
MarioLemus <theliarnorland@gmail.com>
2024-07-02 23:03:33 -0600

    add middlewares

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import { ProductController } from '../controllers/product.controller.js'
+import { validateToken } from '../middlewares/validateToken.js'
+import multer from 'multer'
+import { getMulterStorageConfig } from '../utils/getMulterStorageConfig.js'
 
 const router = Router()
 const productController = new ProductController()
+const storage = await getMulterStorageConfig()
+const upload = multer({ storage })
 
-router.post('/product', productController.create)
-router.put('/product/:_id', productController.edit)
-router.delete('/product/:_id', productController.delete)
+router.post('/product', validateToken, productController.create)
+router.put('/product/:_id', validateToken, upload.single('image'), productController.edit)
+router.delete('/product/:_id', validateToken, productController.delete)
 router.get('/product', productController.get)
 router.get('/product/:name', productController.getbyname)
 router.get('/product/category/:category_id', productController.getbycategory)
